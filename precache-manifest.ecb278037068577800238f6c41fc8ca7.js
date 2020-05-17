ebfc0e255d26f0c224c1d183eb90b2a89b77c011
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-17 11:10:52 +0000

    Deploy tonystrawberry/nike-store-react to github.com/tonystrawberry/nike-store-react.git:gh-pages

---
+++ precache-manifest.ecb278037068577800238f6c41fc8ca7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d71bca82dde30aad665db5622c10335",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f18037e73530f26eb672",
+    "url": "/static/css/3.ea37b057.chunk.css"
+  },
+  {
+    "revision": "6eba7e3f790fe52e750f",
+    "url": "/static/css/main.18f92e36.chunk.css"
+  },
+  {
+    "revision": "fdc3a6adb01499f9910e",
+    "url": "/static/js/2.6082e4e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6082e4e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f18037e73530f26eb672",
+    "url": "/static/js/3.e78d998b.chunk.js"
+  },
+  {
+    "revision": "6eba7e3f790fe52e750f",
+    "url": "/static/js/main.be024832.chunk.js"
+  },
+  {
+    "revision": "40ecb5c41bb2df46114a",
+    "url": "/static/js/runtime-main.b564fb27.js"
+  },
+  {
+    "revision": "ae765ba3e100f35ef0a85b5b280e337e",
+    "url": "/static/media/loading.ae765ba3.gif"
+  },
+  {
+    "revision": "118961ea123aa2fa6165949cfb643078",
+    "url": "/static/media/nike_airforce-large.118961ea.jpg"
+  },
+  {
+    "revision": "1768a94b02ed56df0449bcf30d8cd91c",
+    "url": "/static/media/nike_airforce-medium.1768a94b.jpg"
+  },
+  {
+    "revision": "4c58725d96b3768f44965cce4ccf3701",
+    "url": "/static/media/nike_logo.4c58725d.svg"
+  }
+]);
